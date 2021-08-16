79fcb289482d46d6d6041502664f95f82c9b9ed9
Pedro Morim <1180798@isep.ipp.pt>
2021-08-16 19:02:31 +0100

    Fixed Tooltip error

--- frontend/src/components/Tool/Tool.jsx
+++ frontend/src/components/Tool/Tool.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import { Tooltip, IconButton } from '@material-ui/core';
 
-export const Tool = ({ name, icon, onClick, disabled, ...rest }) => {
-  return (
-    <Tooltip title={name} arrow>
+export const Tool = ({ name, icon, onClick, disabled, ...rest }) => (
+  <Tooltip title={name} arrow>
+    <span>
       <IconButton
         onClick={onClick}
         aria-label={name}
@@ -13,6 +12,6 @@ export const Tool = ({ name, icon, onClick, disabled, ...rest }) => {
       >
         {icon}
       </IconButton>
-    </Tooltip>
-  );
-};
+    </span>
+  </Tooltip>
+);
