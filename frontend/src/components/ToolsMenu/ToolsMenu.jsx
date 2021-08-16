ab1ae0c0cf1bd12bc2a4771a5184be0c107aa676
Pedro Morim <1180798@isep.ipp.pt>
2021-08-16 19:04:23 +0100

    Generalized tool display

--- frontend/src/components/ToolsMenu/ToolsMenu.jsx
+++ frontend/src/components/ToolsMenu/ToolsMenu.jsx
@@ -1,4 +1,6 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 import { Tool } from '../Tool';
 
@@ -9,10 +11,12 @@ import RedoIcon from '@material-ui/icons/Redo';
 import DeleteIcon from '@material-ui/icons/Delete';
 import RotateLeftIcon from '@material-ui/icons/RotateLeft';
 import RotateRightIcon from '@material-ui/icons/RotateRight';
+import SaveIcon from '@material-ui/icons/Save';
 import { useStyles } from './ToolsMenu.styles';
 
 export const ToolsMenu = ({ schematic, selection, history }) => {
   const classes = useStyles();
+  const { id } = useParams();
 
   // Deletes all of the currently selected elements
   const deleteSelection = useCallback(() => {
@@ -30,40 +34,77 @@ export const ToolsMenu = ({ schematic, selection, history }) => {
     [schematic, selection.selectedItems],
   );
 
+  // Save the current circuit to the database
+  const saveCircuit = useCallback(async () => {
+    try {
+      // await axios.patch('/');
+      console.log('TODO: Save the circuit');
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  const tools = useMemo(
+    () => [
+      [
+        {
+          name: 'Undo',
+          icon: <UndoIcon />,
+          onClick: history.undo,
+          disabled: !history.canUndo,
+        },
+        {
+          name: 'Redo',
+          icon: <RedoIcon />,
+          onClick: history.redo,
+          disabled: !history.canRedo,
+        },
+      ],
+      [
+        {
+          name: 'Delete',
+          icon: <DeleteIcon />,
+          onClick: deleteSelection,
+          disabled: !selection.selectedItems.size,
+        },
+      ],
+      [
+        {
+          name: 'Rotate Left',
+          icon: <RotateLeftIcon />,
+          onClick: () => rotateSelection(-90),
+          disabled: !selection.selectedItems.size,
+        },
+        {
+          name: 'Rotate Right',
+          icon: <RotateRightIcon />,
+          onClick: () => rotateSelection(-90),
+          disabled: !selection.selectedItems.size,
+        },
+      ],
+      [
+        {
+          name: 'Save',
+          icon: <SaveIcon />,
+          onClick: saveCircuit,
+        },
+      ],
+    ],
+    [selection, history, deleteSelection, rotateSelection, saveCircuit],
+  );
+
   return (
     <Grid container direction='row' justifyContent='center' alignItems='center'>
-      <Tool
-        name='Undo'
-        icon={<UndoIcon />}
-        onClick={history.undo}
-        disabled={!history.canUndo}
-      />
-      <Tool
-        name='Redo'
-        icon={<RedoIcon />}
-        onClick={history.redo}
-        disabled={!history.canRedo}
-      />
-      <Divider className={classes.divider} orientation='vertical' />
-      <Tool
-        name='Delete'
-        icon={<DeleteIcon />}
-        onClick={deleteSelection}
-        disabled={!selection.selectedItems.size}
-      />
-      <Divider className={classes.divider} orientation='vertical' />
-      <Tool
-        name='Rotate Left'
-        icon={<RotateLeftIcon />}
-        onClick={() => rotateSelection(-90)}
-        disabled={!selection.selectedItems.size}
-      />
-      <Tool
-        name='Rotate Right'
-        icon={<RotateRightIcon />}
-        onClick={() => rotateSelection(+90)}
-        disabled={!selection.selectedItems.size}
-      />
+      {tools.map((menu, menuId) => (
+        <React.Fragment key={menuId}>
+          {menuId > 0 && (
+            <Divider className={classes.divider} orientation='vertical' />
+          )}
+          {menu.map((tool, toolId) => (
+            <Tool key={toolId} {...tool} />
+          ))}
+        </React.Fragment>
+      ))}
     </Grid>
   );
 };
