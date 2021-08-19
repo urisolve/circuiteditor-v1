8deaeac93bc2cb796326cfffaf170b4495adfe5c
Pedro Morim <1180798@isep.ipp.pt>
2021-08-19 23:37:23 +0100

    Added ThemeProvider for Material-UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
+
+import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { theme } from './theme';
+
 import { App } from './App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <CssBaseline />
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
