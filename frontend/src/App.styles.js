50d3d2dc36952f7e4f6a13d071ce984403ade8fc
Pedro Morim <1180798@isep.ipp.pt>
2021-08-20 16:33:24 +0100

    Changed scrollbar style

--- frontend/src/App.styles.js
+++ frontend/src/App.styles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
 
 export const useStyles = makeStyles((theme) => ({
   '@global': {
@@ -7,10 +8,14 @@ export const useStyles = makeStyles((theme) => ({
       height: 10,
     },
     '*::-webkit-scrollbar-track': {
-      background: 'white',
+      display: 'none',
     },
     '*::-webkit-scrollbar-thumb': {
-      background: 'grey',
+      borderRadius: theme.spacing(2),
+      background: grey[500],
+      '&:hover': {
+        background: grey[400],
+      },
     },
     html: {
       scrollBehavior: 'smooth',
