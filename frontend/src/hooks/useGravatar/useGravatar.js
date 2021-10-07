4a1facdede28231e95813cf39cebbd8577cccc87
Pedro Morim <1180798@isep.ipp.pt>
2021-10-07 10:58:02 +0100

    Updated gravatar's default image

--- frontend/src/hooks/useGravatar/useGravatar.js
+++ frontend/src/hooks/useGravatar/useGravatar.js
@@ -2,15 +2,15 @@ import { useMemo } from 'react';
 import CryptoJS from 'crypto-js';
 import lodash from 'lodash';
 
-export const useGravatar = (email, size = 256) => {
+export const useGravatar = (email, size = 256, d = 'robohash') => {
   const gravatar = useMemo(() => {
     const trimmedEmail = lodash.trim(email);
     const lowercaseEmail = trimmedEmail.toLowerCase();
     const hash = CryptoJS.MD5(lowercaseEmail).toString();
-    const url = `https://www.gravatar.com/avatar/${hash}?d=identicon&s=${size}`;
+    const url = `https://www.gravatar.com/avatar/${hash}?d=${d}&s=${size}`;
 
     return url;
-  }, [email, size]);
+  }, [email, size, d]);
 
   return gravatar;
 };
