c6a607d662ad5eae06f6058a2791c5a1b39e763b
Pedro Morim <1180798@isep.ipp.pt>
2021-12-09 11:55:25 +0000

    Removed random whitespace

--- frontend/src/hooks/useSchematic/useSchematic.js
+++ frontend/src/hooks/useSchematic/useSchematic.js
@@ -1,7 +1,7 @@
 import { useState, useCallback, useMemo, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import lodash from 'lodash';
-  
+
 import {
   isComponent,
   isConnection,
