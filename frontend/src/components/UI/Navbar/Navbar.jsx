0d4f37a12b3e03bb3e3c751ac548e1a5c46c6571
Pedro Morim <1180798@isep.ipp.pt>
2021-12-16 14:42:48 +0000

    Fixed Navbar positioning

--- frontend/src/components/UI/Navbar/Navbar.jsx
+++ frontend/src/components/UI/Navbar/Navbar.jsx
@@ -55,132 +55,135 @@ export function Navbar({ ...rest }) {
   const sm = useMediaQuery((theme) => theme.breakpoints.up('sm'));
 
   return (
-    <AppBar
-      position='sticky'
-      color='primary'
-      sx={{ width: 1, zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      {...rest}
-    >
-      <Container>
-        <Toolbar>
-          <Button
-            component={Link}
-            to='/'
-            sx={{
-              p: 0,
-              color: 'white',
-              textDecoration: 'none',
-            }}
-          >
-            <Logo style={{ width: 60 }} />
+    <>
+      <AppBar
+        position='fixed'
+        color='primary'
+        sx={{ width: 1, zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        {...rest}
+      >
+        <Container>
+          <Toolbar>
+            <Button
+              component={Link}
+              to='/'
+              sx={{
+                p: 0,
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              <Logo style={{ width: 60 }} />
 
-            <Hidden smDown>
-              <Typography
-                variant='h1'
-                noWrap
-                sx={{ ml: 2, fontSize: 24, fontWeight: 'regular' }}
-              >
-                Circuit Editor
-              </Typography>
-            </Hidden>
-          </Button>
+              <Hidden smDown>
+                <Typography
+                  variant='h1'
+                  noWrap
+                  sx={{ ml: 2, fontSize: 24, fontWeight: 'regular' }}
+                >
+                  Circuit Editor
+                </Typography>
+              </Hidden>
+            </Button>
 
-          <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ flexGrow: 1 }} />
 
-          {user ? (
-            sm ? (
-              <Button
-                ref={anchorEl}
-                onClick={isMenuOpen.on}
-                startIcon={
+            {user ? (
+              sm ? (
+                <Button
+                  ref={anchorEl}
+                  onClick={isMenuOpen.on}
+                  startIcon={
+                    <Avatar
+                      alt={`${user.firstName} ${user.lastName}`}
+                      src={gravatar}
+                    />
+                  }
+                  variant='contained'
+                  color='primary'
+                  disableElevation
+                  disableFocusRipple
+                  style={{ backgroundColor: 'transparent' }}
+                >
+                  {`${user.firstName} ${user.lastName}`}
+                </Button>
+              ) : (
+                <IconButton ref={anchorEl} onClick={isMenuOpen.on}>
                   <Avatar
                     alt={`${user.firstName} ${user.lastName}`}
                     src={gravatar}
                   />
-                }
-                variant='contained'
-                color='primary'
-                disableElevation
-                disableFocusRipple
-                style={{ backgroundColor: 'transparent' }}
-              >
-                {`${user.firstName} ${user.lastName}`}
-              </Button>
+                </IconButton>
+              )
             ) : (
-              <IconButton ref={anchorEl} onClick={isMenuOpen.on}>
-                <Avatar
-                  alt={`${user.firstName} ${user.lastName}`}
-                  src={gravatar}
-                />
-              </IconButton>
-            )
-          ) : (
-            <Button
-              component={Link}
-              to='/auth'
-              sx={{ textDecoration: 'none' }}
-              color='inherit'
-              variant='outlined'
-            >
-              Login
-            </Button>
-          )}
-        </Toolbar>
-      </Container>
+              <Button
+                component={Link}
+                to='/auth'
+                sx={{ textDecoration: 'none' }}
+                color='inherit'
+                variant='outlined'
+              >
+                Login
+              </Button>
+            )}
+          </Toolbar>
+        </Container>
 
-      <Menu
-        anchorEl={anchorEl.current}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={isMenuOpen.value}
-        onClose={isMenuOpen.off}
-        keepMounted
-      >
-        <MenuItem
-          component={Link}
-          to='/circuits'
-          onClick={isMenuOpen.off}
-          sx={menuLink}
-        >
-          <MenuIcon sx={{ mr: 2 }} />
-          My Circuits
-        </MenuItem>
-        <MenuItem
-          component={Link}
-          to='/account'
-          onClick={isMenuOpen.off}
-          sx={menuLink}
-        >
-          <PersonIcon sx={{ mr: 2 }} />
-          Account
-        </MenuItem>
-        <MenuItem
-          disabled
-          component={Link}
-          to='/settings'
-          onClick={isMenuOpen.off}
-          sx={menuLink}
-        >
-          <SettingsIcon sx={{ mr: 2 }} />
-          Settings
-        </MenuItem>
-        <Divider />
-        <MenuItem
-          onClick={() => {
-            isMenuOpen.off();
-            logOut();
+        <Menu
+          anchorEl={anchorEl.current}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
           }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={isMenuOpen.value}
+          onClose={isMenuOpen.off}
+          keepMounted
         >
-          <ExitToAppIcon sx={{ mr: 2 }} />
-          Logout
-        </MenuItem>
-      </Menu>
-    </AppBar>
+          <MenuItem
+            component={Link}
+            to='/circuits'
+            onClick={isMenuOpen.off}
+            sx={menuLink}
+          >
+            <MenuIcon sx={{ mr: 2 }} />
+            My Circuits
+          </MenuItem>
+          <MenuItem
+            component={Link}
+            to='/account'
+            onClick={isMenuOpen.off}
+            sx={menuLink}
+          >
+            <PersonIcon sx={{ mr: 2 }} />
+            Account
+          </MenuItem>
+          <MenuItem
+            disabled
+            component={Link}
+            to='/settings'
+            onClick={isMenuOpen.off}
+            sx={menuLink}
+          >
+            <SettingsIcon sx={{ mr: 2 }} />
+            Settings
+          </MenuItem>
+          <Divider />
+          <MenuItem
+            onClick={() => {
+              isMenuOpen.off();
+              logOut();
+            }}
+          >
+            <ExitToAppIcon sx={{ mr: 2 }} />
+            Logout
+          </MenuItem>
+        </Menu>
+      </AppBar>
+      <Toolbar />
+    </>
   );
 }
