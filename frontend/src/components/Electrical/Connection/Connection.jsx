849013a268bef913277ed1bf42f260a686d6a0b7
Pedro Morim <1180798@isep.ipp.pt>
2021-12-16 11:49:39 +0000

    Fixed connection's ref forwarding

--- frontend/src/components/Electrical/Connection/Connection.jsx
+++ frontend/src/components/Electrical/Connection/Connection.jsx
@@ -1,29 +1,24 @@
+import { forwardRef } from 'react';
 import XArrow from 'react-xarrows';
 
-export function Connection({
-  start,
-  end,
-  type,
-  properties,
-  onClick,
-  isSelected,
-  ...rest
-}) {
-  return (
-    <XArrow
-      start={start}
-      end={end}
-      path={type ?? 'grid'}
-      showHead={false}
-      gridBreak={properties?.gridBreak ?? '100%'}
-      startAnchor='middle'
-      endAnchor='middle'
-      divContainerStyle={{
-        zIndex: -1,
-        opacity: properties?.opacity ?? 1,
-      }}
-      passProps={{ onClick }}
-      {...rest}
-    />
-  );
-}
+export const Connection = forwardRef(
+  ({ start, end, type, properties, onClick, isSelected, ...rest }, ref) => {
+    return (
+      <XArrow
+        start={start}
+        end={end}
+        path={type ?? 'grid'}
+        showHead={false}
+        gridBreak={properties?.gridBreak ?? '100%'}
+        startAnchor='middle'
+        endAnchor='middle'
+        divContainerStyle={{
+          zIndex: -1,
+          opacity: properties?.opacity ?? 1,
+        }}
+        passProps={{ onClick }}
+        {...rest}
+      />
+    );
+  },
+);
