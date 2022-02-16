1f7719ed84b47f1911d146a3b7e03adaf4be62e9
Pedro Morim <1180798@isep.ipp.pt>
2022-02-16 17:37:30 +0000

    Added Labels to connections

--- frontend/src/components/Electrical/Connection/Connection.jsx
+++ frontend/src/components/Electrical/Connection/Connection.jsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material';
 import XArrow from 'react-xarrows';
+import { Label } from '..';
 
 import {
   useContextMenu,
@@ -16,6 +17,8 @@ export function Connection({
   label,
   properties,
   isSelected,
+  canvasRef,
+  updatePosition,
   ...rest
 }) {
   const refMap = useGlobalRefMap(id);
@@ -41,6 +44,15 @@ export function Connection({
           onContextMenu: contextMenu.open,
           onDoubleClick: propertiesMenu.open,
         }}
+        labels={
+          <Label
+            owner={id}
+            canvasRef={canvasRef}
+            updatePosition={updatePosition}
+            onDoubleClick={() => propertiesMenu.openTab(1)}
+            {...label}
+          />
+        }
         {...rest}
       />
       <ContextMenu id={id} {...contextMenu} />
