9472948bd16f547f338ddef012dbb1a13b4e99c6
Pedro Morim <1180798@isep.ipp.pt>
2022-04-03 18:29:03 +0100

    Change units to ASCII values

--- frontend/src/configs/components.config.js
+++ frontend/src/configs/components.config.js
@@ -79,7 +79,7 @@ export const components = Object.freeze({
     fullName: 'Inductor',
     label: {
       name: 'L',
-      value: '1µ',
+      value: '1u',
       unit: 'F',
       position: { x: 10, y: 0 },
     },
@@ -91,7 +91,7 @@ export const components = Object.freeze({
     label: {
       name: 'Z',
       value: '10',
-      unit: 'Ω',
+      unit: 'Ohm',
       position: { x: 10, y: 0 },
     },
     ports: [{ position: { x: 0, y: 0.5 } }, { position: { x: 1, y: 0.5 } }],
@@ -102,7 +102,7 @@ export const components = Object.freeze({
     label: {
       name: 'R',
       value: '10k',
-      unit: 'Ω',
+      unit: 'Ohm',
       position: { x: 10, y: 0 },
     },
     ports: [{ position: { x: 0, y: 0.5 } }, { position: { x: 1, y: 0.5 } }],
