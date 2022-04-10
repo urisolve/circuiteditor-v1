e59fcdd7055dae24445681e45587d578c45b7056
Pedro Morim <1180798@isep.ipp.pt>
2022-04-10 18:50:28 +0100

    Fix interconnected nodes bug

--- frontend/src/hooks/useNetlist/useNetlist.js
+++ frontend/src/hooks/useNetlist/useNetlist.js
@@ -17,36 +17,6 @@ import { components } from '../../configs/components.config';
 const version = process.env.REACT_APP_VERSION ?? '1.0.0';
 const netlistHeader = `# CircuitEditor v${version}\n\n`;
 
-function splitPorts(schematic) {
-  for (const component of schematic.components) {
-    for (const port of component.ports) {
-      // Grab all connections to the port
-      const connections = schematic.connections.filter((connection) =>
-        isConnected(port, connection),
-      );
-
-      // If the port had more than 1 connection, split it into a node
-      if (connections.length > 1) {
-        // Create a new node
-        const newNode = { id: uuidv4() };
-        schematic.nodes.push(newNode);
-
-        // Connect the port to the new node
-        schematic.connections.push({
-          id: uuidv4(),
-          start: port.id,
-          end: newNode.id,
-        });
-
-        // Move old port's connections to the new node
-        for (const connection of connections) {
-          moveConnection(connection, port, newNode);
-        }
-      }
-    }
-  }
-}
-
 function applyGround(schematic) {
   for (const component of schematic.components) {
     // Ignore non-ground components
@@ -82,22 +52,20 @@ function condenseNodes(schematic) {
         .some(Boolean),
     );
 
-    // If the node is isolated, ignore it
+    // If there are no interconnected nodes, skip
     if (!connectedNodes.length) continue;
 
-    // Move connections to new node
-    // TODO: Also move the label
     for (const connectedNode of connectedNodes) {
+      // Move connections to new node
       for (const connection of schematic.connections) {
-        if (isConnected(connectedNode, connection)) {
+        if (isConnected(connectedNode, connection) && connection.id) {
           moveConnection(connection, connectedNode, node);
+          // TODO: Also move the label
         }
       }
-    }
 
-    // Delete old nodes
-    for (const connectedNode of connectedNodes) {
-      const idx = connectedNodes.indexOf(connectedNode);
+      // Delete the now useless node
+      const idx = schematic.nodes.indexOf(connectedNode);
       schematic.nodes.splice(idx, 1);
     }
 
@@ -146,16 +114,15 @@ function generateNodesString(component, schematic) {
   let nodeStr = '';
 
   for (const port of component.ports) {
+    // Search for the node (virtual or real) that is connected to the port
     const conn = schematic.connections.find((conn) => isConnected(port, conn));
+    const node = schematic.nodes.find((node) => isConnected(node, conn));
 
-    if (!conn) {
+    if (!conn || !node) {
       throw new Error(
         `Component with ID "${component.id}" is not fully connected. Make sure all ports of the component are connected somewhere.`,
       );
     }
-
-    // Search for the node (virtual or real) that is connected to the port
-    const node = schematic.nodes.find((node) => isConnected(node, conn));
 
     // Generate a name if the node doesn't have one
     const uniqueName = generateUniqueName(
@@ -163,6 +130,7 @@ function generateNodesString(component, schematic) {
       LabelOptions.NUMERIC,
       '_net',
     );
+
     if (!node.label) node.label = { name: uniqueName };
     else if (!node.label.name) node.label.name = uniqueName;
 
@@ -189,7 +157,6 @@ export function useNetlist(sch) {
     const schematic = lodash.cloneDeep(sch);
 
     // Normalize the schematic's data model
-    splitPorts(schematic);
     condenseNodes(schematic);
     applyGround(schematic);
     withVirtualNodes(schematic);
@@ -205,7 +172,11 @@ export function useNetlist(sch) {
         const valueStr = generateValueString(component);
 
         netlist += `${nameStr} ${nodesStr} ${valueStr}\n`;
-      } catch (error) {}
+      } catch (error) {
+        if (process.env.REACT_APP_NETLIST_DEBUG) {
+          console.error(error);
+        }
+      }
     }
 
     return netlist;
