ef5ac7e7fac1a05e3ee46a0c43ef2b34250da916
Pedro Morim <1180798@isep.ipp.pt>
2022-06-08 20:03:12 +0100

    Fix Capacitor and Inductor units

--- frontend/src/electrical/C.js
+++ frontend/src/electrical/C.js
@@ -4,7 +4,7 @@ export const C = {
   label: {
     name: 'C',
     value: '10m',
-    unit: 'H',
+    unit: 'F',
     position: { x: 10, y: 0 },
   },
   ports: [{ position: { x: 0, y: 0.5 } }, { position: { x: 1, y: 0.5 } }],
--- frontend/src/electrical/L.js
+++ frontend/src/electrical/L.js
@@ -4,7 +4,7 @@ export const L = {
   label: {
     name: 'L',
     value: '1u',
-    unit: 'F',
+    unit: 'H',
     position: { x: 10, y: 0 },
   },
   ports: [{ position: { x: 0, y: 0.5 } }, { position: { x: 1, y: 0.5 } }],
